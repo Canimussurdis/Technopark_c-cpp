1b23f480d6e88d079dfdce1cd019370a0ace7330
Canimussurdis <76573311+Canimussurdis@users.noreply.github.com>
2021-11-03 23:37:22 +0300

    Update utils.c

--- src/utils.c
+++ src/utils.c
@@ -14,39 +14,40 @@ void free_arr(void** ptr, int length) {
 
 int read_file(char*** p_str_arr, const char* fpath) {
     int current_size = BASE_LINES_AMOUNT;
-    char** c_data = *p_str_arr;
-    c_data = malloc(current_size * sizeof(*c_data));
+    char** comment_data = *p_str_arr;
+    comment_data = malloc(current_size * sizeof(*comment_data));
 
     FILE* fp = fopen(fpath, "r");
 
     int32_t ln_amount = 0;
-    int s_len = MAX_LINE_LEN;
+    int string_len = MAX_LINE_LEN;
     char* res;
     do {
         if (ln_amount >= current_size) {
             current_size *= 2;
-            char** new_ptr = realloc(c_data, current_size * sizeof(*c_data));
+            char** new_ptr = realloc(comment_data, current_size * sizeof(*comment_data));
             if (new_ptr == NULL) {
-                free_arr((void**)c_data, ln_amount);
-                free(c_data);
+                free_arr((void**)comment_data, ln_amount);
+                free(comment_data);
                 return -2;
             }
-            c_data = new_ptr;
+            comment_data = new_ptr;
         }
-        c_data[ln_amount] = malloc(s_len * sizeof(**c_data));
-        if (c_data[ln_amount] == NULL) {
-            free_arr((void**)c_data, ln_amount);
-            free(c_data);
+        comment_data[ln_amount] = malloc(string_len * sizeof(**comment_data));
+        if (comment_data[ln_amount] == NULL) {
+            free_arr((void**)comment_data, ln_amount);
+            free(comment_data);
             return -2;
         }
-        res = fgets(c_data[ln_amount], s_len, fp);
+        res = fgets(comment_data[ln_amount], string_len, fp);
         ln_amount++;
     } while (res != NULL);
     ln_amount--;
-    free(c_data[ln_amount]);  // the last el is NULL
+    free(comment_data[ln_amount]);  
     fclose(fp);
 
-    *p_str_arr = c_data;
+    *p_str_arr = comment_data;
 
     return ln_amount;
 }
+

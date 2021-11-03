b239febae957a8b40f44ea8963c2af39289cb153
Canimussurdis <76573311+Canimussurdis@users.noreply.github.com>
2021-11-03 23:32:54 +0300

    Update random_data_gen.h

--- src/include/random_data_gen.h
+++ src/include/random_data_gen.h
@@ -13,3 +13,4 @@ int random_data_string(char* string, int s_len, unsigned* rseed);
 int create_random_data_file(const char* fpath, int64_t amnt, unsigned* rseed);
 
 #endif  // SRC_INCLUDE_RANDOM_DATA_CREATION_H_
+

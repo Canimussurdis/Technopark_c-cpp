e67c1c7fa2310d563af7115885645e7ef052d834
Canimussurdis <76573311+Canimussurdis@users.noreply.github.com>
2021-11-04 02:45:04 +0300

    Update random_data_gen.h

--- src/include/random_data_gen.h
+++ src/include/random_data_gen.h
@@ -11,7 +11,7 @@
 void random_data(struct comment_data* comment, int rand_years, unsigned* rseed);
 
 // print comment string filled with random data
-int random_data_string(char* string, int s_len, unsigned* rseed);
+int random_data_string(char* str, int s_len, unsigned* rseed);
 
 // create file with random data
 int create_random_data_file(const char* fpath, int64_t amnt, unsigned* rseed);

470c212bdbb7e0148eb2f51a660eaa8268b961d3
Canimussurdis <76573311+Canimussurdis@users.noreply.github.com>
2021-11-03 23:17:33 +0300

    Update comment_data.h

--- src/include/comment_data.h
+++ src/include/comment_data.h
@@ -10,14 +10,17 @@ struct comment_data {
     int id;
     float average_score;
     int score_amount;
-    struct date ld;
+    struct date last_date;
     int last_score;
 };
 
-bool parse_comment(struct comment_data* c, char* s);
+// parsing comment
+bool parse_comment(struct comment_data* comment, char* string);
 
-bool is_comment_in_last_q(const struct comment_data c);
+// check if comment is being sent in last quater 
+bool is_comment_in_last_quater(const struct comment_data comment);
 
+// count how many comments were in last quater
 int count_actual_comments(const char* fpath, int avg_score);
 
 #endif  // SRC_INCLUDE_COMMENT_DATA_H_

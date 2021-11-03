f88db3619892455f568954d3b2e1b4560f23fe79
Canimussurdis <76573311+Canimussurdis@users.noreply.github.com>
2021-11-03 23:18:53 +0300

    Update utils.h

--- src/include/utils.h
+++ src/include/utils.h
@@ -2,8 +2,10 @@
 #define SRC_INCLUDE_UTILS_H_
 
 
+// free an array
 void free_arr(void** ptr, int length);
 
+// read file with data
 int read_file(char*** p_str_arr, const char* fpath);
 
 #endif  // SRC_INCLUDE_UTILS_H_
